3fb0af4385faa791f8d1590ae52b356e334599dd
Burgy Benjamin <minidfx@gmail.com>
2015-10-26 12:35:30 +0100

    Can read OpenStack and Amazon webservice using libcloud.

--- delivrable1.py
+++ delivrable1.py
@@ -1,20 +1,24 @@
 import getpass
+
+from settings import *
+
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
 
 # Get the OpenStack drivers
 openStack = get_driver(Provider.OPENSTACK)
 
-user = 'benjamin.burgy@master.hes-so.ch'
-password = getpass.getpass(prompt='Password: ', stream=None)
-tenant = '43303a8189a04df387c58c7499ced0bc'
-
 # Init the driver
 driver = openStack(user,
                    password,
-                   ex_tenant_name = "benjamin.burgy@master.hes-so.ch",
-                   ex_force_auth_url = 'https://keystone.cloud.switch.ch:5000/v2.0/tokens',
+                   ex_tenant_name = tenant_name,
+                   ex_force_auth_url = auth_url,
                    ex_force_auth_version = '2.0_password',
-                   ex_force_service_region = 'ZH')
+                   ex_force_service_region = service_region)
+
+print driver.list_nodes()
+
+amazon = get_driver(Provider.EC2_EU_WEST)
+driver = amazon(accesskey, secretkey)
 
 print driver.list_nodes()
