4ad58714dc0fd8dfd464f804ca356328beed1ff2
Burgy Benjamin <minidfx@gmail.com>
2015-10-31 12:06:02 +0100

    Add a base class for OpenStack and Amazon.

---
+++ Cloud.py
@@ -0,0 +1,17 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Cloud(object):
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        self.driver = None
+        self.activeNode = None
+
+    @abstractmethod
+    def create(self):
+        raise Exception('Not implemented')
+
+    @abstractmethod
+    def destroy(self):
+        raise Exception('Not implemented')
